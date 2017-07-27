151faa2f2eae6018fb33ec20abb64abbd502f0cf
zhuyuwei <zhuyuwei@dianwoba.com>
2017-07-27 15:09:32 +0800

    refine

--- etc/config.js
+++ etc/config.js
@@ -1,8 +1,8 @@
 export default {
 	// 线上环境
-	basePath: 'https://www.skyvow.cn/api', 
-	domain: 'https://www.skyvow.cn/', 
+	// basePath: 'https://www.nicegm.cn/api', 
+	// domain: 'https://www.nicegm.cn/', 
 	// 本地环境
-	// basePath: 'http://localhost:3000/api', 
-	// domain: 'http://localhost:3000/', 
+	basePath: 'http://localhost:8081/api', 
+	domain: 'http://localhost:8081/', 
 }
